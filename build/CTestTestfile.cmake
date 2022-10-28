82efc817da626b518b92c225d6ee58acc1bbf88c
lzx <niebayes@gmail.com>
2022-10-28 20:43:58 +0800

    remove irrevelant files

--- build/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /Users/lzz/graph_algos
-# Build directory: /Users/lzz/graph_algos/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
